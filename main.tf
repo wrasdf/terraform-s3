96f80ccd7d7b470a8b57b5bef4ebde27e3dd1ad8
wrasdf <452784+wrasdf@users.noreply.github.com>
2026-05-01 15:15:52 +1000

    terraform for AWS S3 module

---
+++ main.tf
@@ -0,0 +1,119 @@
+data "aws_region" "current" {
+  region = var.region
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_partition" "current" {}
+
+resource "aws_s3_bucket" "this" {
+  count = var.create_bucket ? 1 : 0
+
+  region = var.region
+
+  bucket           = var.bucket_name
+  bucket_prefix    = var.bucket_prefix
+  bucket_namespace = var.bucket_namespace
+
+  force_destroy       = var.force_destroy
+  object_lock_enabled = var.object_lock_enabled
+  tags                = var.tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt" {
+  count  = var.create_bucket ? 1 : 0
+  bucket = aws_s3_bucket.this[0].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = var.kms_key_arn != null ? "aws:kms" : "AES256"
+      kms_master_key_id = var.kms_key_arn != null ? var.kms_key_arn : null
+    }
+    
+    # Best practice: Enable bucket key to reduce KMS costs if using KMS
+    bucket_key_enabled = var.kms_key_arn != null ? true : null
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  count = var.create_bucket ? 1 : 0
+
+  region = var.region
+
+  bucket = aws_s3_bucket.this[0].id
+
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+  skip_destroy            = var.skip_destroy_public_access_block
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  count = var.create_bucket && var.bucket_policy_json != null ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+  policy = var.bucket_policy_json
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  count = var.create_bucket && var.enable_versioning ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
+  count = var.create_bucket && length(var.lifecycle_rules) > 0 ? 1 : 0
+
+  bucket = aws_s3_bucket.this.id
+
+  dynamic "rule" {
+    for_each = var.lifecycle_rules
+
+    content {
+      id     = try(rule.value.id, null)
+      status = rule.value.status
+
+      dynamic "abort_incomplete_multipart_upload" {
+        for_each = try([rule.value.abort_incomplete_multipart_upload_days], [])
+
+        content {
+          days_after_initiation = try(rule.value.abort_incomplete_multipart_upload_days, null)
+        }
+      }
+
+      dynamic "expiration" {
+        for_each = try(flatten([rule.value.expiration]), [])
+
+        content {
+          date                         = try(expiration.value.date, null)
+          days                         = try(expiration.value.days, null)
+          expired_object_delete_marker = try(expiration.value.expired_object_delete_marker, null)
+        }
+      }
+
+      dynamic "transition" {
+        for_each = try(flatten([rule.value.transition]), [])
+
+        content {
+          date          = try(transition.value.date, null)
+          days          = try(transition.value.days, null)
+          storage_class = transition.value.storage_class
+        }
+      }
+
+      dynamic "noncurrent_version_expiration" {
+        for_each = try(flatten([rule.value.noncurrent_version_expiration]), [])
+
+        content {
+          newer_noncurrent_versions = try(noncurrent_version_expiration.value.newer_noncurrent_versions, null)
+          noncurrent_days           = try(noncurrent_version_expiration.value.days, noncurrent_version_expiration.value.noncurrent_days, null)
+        }
+      }
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,99 @@
+variable "create_bucket" {
+  description = "Controls if S3 bucket should be created"
+  type        = bool
+  default     = true
+}
+
+variable "region" {
+  description = "Region where the resource(s) will be managed."
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "bucket_name" {
+  description = "Name of the bucket"
+  type        = string
+}
+
+variable "bucket_prefix" {
+  description = "(Optional, Forces new resource) Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket."
+  type        = string
+  default     = null
+}
+
+variable "bucket_namespace" {
+  description = "Namespace for the bucket. Determines bucket naming scope. Valid values: account-regional, global. Defaults to global (AWS)"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "Resource tagging"
+  type        = map(string)
+}
+
+variable "force_destroy" {
+  description = "Whether force destroy to bucket"
+  type        = bool
+  default     = false
+}
+
+variable "object_lock_enabled" {
+  description = "Whether S3 bucket should have an Object Lock configuration enabled."
+  type        = bool
+  default     = false
+}
+
+variable "block_public_acls" {
+  description = "Whether Amazon S3 should block public ACLs for this bucket."
+  type        = bool
+  default     = true
+}
+
+variable "block_public_policy" {
+  description = "Whether Amazon S3 should block public bucket policies for this bucket."
+  type        = bool
+  default     = true
+}
+
+variable "ignore_public_acls" {
+  description = "Whether Amazon S3 should ignore public ACLs for this bucket."
+  type        = bool
+  default     = true
+}
+
+variable "restrict_public_buckets" {
+  description = "Whether Amazon S3 should restrict public bucket policies for this bucket."
+  type        = bool
+  default     = true
+}
+
+variable "skip_destroy_public_access_block" {
+  description = "Whether to skip destroying the S3 Bucket Public Access Block configuration when destroying the bucket. Only used if `public_access_block` is set to true."
+  type        = bool
+  default     = true
+}
+
+variable "bucket_policy_json" {
+  description = "Policy to apply to the bucket"
+  type        = string
+  default     = null
+}
+
+variable "enable_versioning" {
+  description = "Whether enable object versioning to the bucket"
+  type        = bool
+  default     = true
+}
+
+variable "kms_key_arn" {
+  description = "KMS key ARN for service side encryption"
+  type        = string
+  default     = null
+}
+
+variable "lifecycle_rules" {
+  description = "List of rule objects for bucket lifecycle"
+  type        = any
+  default     = []
+}
